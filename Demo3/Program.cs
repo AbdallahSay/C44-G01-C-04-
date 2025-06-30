5f3da281cc5206a57f567a276b6aa116d4a45937
AbdallahSay <luka3122003@gmail.com>
2025-06-30 17:42:51 +0300

    Conditional Statements Examples

--- Demo3/Program.cs
+++ Demo3/Program.cs
@@ -4,7 +4,110 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Conditional Statements EX1:
+            //if (age > 22)
+            //    Console.WriteLine($"Your age > 22 = {age}");
+            //else if (age < 22)
+            //    Console.WriteLine($"Your age < 22 = {age}");
+            //else
+            //    Console.WriteLine($"Your age = 22");
+            //Console.WriteLine("Please Enter Your Age"); // Relational Pattern -- No jump table
+            //int.TryParse(Console.ReadLine(), out int age);
+            //switch (age)
+            //{
+            //    case (< 22):
+            //    Console.WriteLine($"Your age > 22 = {age}");
+            //        break;
+            //    case (> 22):
+            //        Console.WriteLine($"Your age > 22 = {age}");
+            //        break;
+            //    default:
+            //              Console.WriteLine($"Your age > 22 = {age}");
+            //        break;
+
+
+
+
+            //}
+
+
+            #endregion
+            #region  Conditional Statements EX2(Name -- Hello)
+            //if (name == "Abdallah")
+            //    Console.WriteLine("Hello Abdallah");
+            //else if (name == "Mo")
+            //    Console.WriteLine("Hello Mo");
+            //else
+            //    Console.WriteLine($"Hello {name}");
+            //Console.WriteLine("Please Enter Your Name");
+            //String name = Console.ReadLine();
+            //switch (name)
+            //{
+            //    case ("Abdallah"):
+            //        Console.WriteLine("Hello abdallah");
+            //        break;
+            //    case ("Mo"):
+            //        Console.WriteLine("Hello mo");
+            //        break;
+            //    default:
+            //        Console.WriteLine($"hello {name}");
+            //        break;
+
+            //}
+
+            #endregion
+            #region Conditional Statements EX3
+
+            //if (budget == 1000)
+            //    Console.WriteLine("Option1");
+            //else if (budget == 2000)
+            //{
+            //    Console.WriteLine("Option1");
+            //    Console.WriteLine("Option2");
+            //}
+            //else if (budget == 3000)
+            //{
+            //    Console.WriteLine("Option1");
+            //    Console.WriteLine("Option2");
+            //    Console.WriteLine("Option3");
+
+            //}
+
+            //else
+            //    Console.WriteLine("Unknown");
+
+            //Console.WriteLine("Please Enter Your Budget");
+            //double.TryParse(Console.ReadLine(), out double budget);
+            //switch (budget)
+            //{
+            //    case (1000):
+            //        Console.WriteLine("Option1");
+            //        break;
+            //    case (2000):
+            //            Console.WriteLine("Option2");
+            //        goto case 1000;
+            //    case (3000):
+            //        Console.WriteLine("Option3");
+            //        goto case 2000;
+            //        break;
+            //    default:
+            //        Console.WriteLine("UnKnown");
+            //        break;
+
+
+            //}
+
+        #endregion
+            #region goto
+        //abdol:
+        //    Console.WriteLine("Hello abdol");
+
+        //    Console.WriteLine("please enter num");
+        //    int.TryParse(Console.ReadLine(), out int num);
+        //    goto abdol;
+            #endregion
+
+
         }
     }
 }
