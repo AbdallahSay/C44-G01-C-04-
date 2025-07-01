fbd25d18b4b36cef2835dda080aea0e418cd1ead
AbdallahSay <luka3122003@gmail.com>
2025-07-01 10:30:53 +0300

    q2

--- Demo3/Program.cs
+++ Demo3/Program.cs
@@ -6,13 +6,30 @@
         {
 
             #region 1-Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
-            Console.WriteLine("Plase enter number");
-            bool isParsed = int.TryParse(Console.ReadLine(), out int number);
-            if (isParsed && (number % 3 == 0 || number % 4 == 0))
-                Console.WriteLine("Yes");
-            else
-                Console.WriteLine("No");
+            //Console.WriteLine("Plase enter number");
+            //bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+            //if (isParsed && (number % 3 == 0 || number % 4 == 0))
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
 
+            //#endregion
+
+
+
+            #endregion
+            #region 2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            //Console.WriteLine("please enter number");
+            //bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+            //if (isParsed)
+            //{
+            //    if(number > 0 )
+            //        Console.WriteLine("Positive");
+            //    else if (number < 0)
+            //        Console.WriteLine("Negative");
+            //    else
+            //        Console.WriteLine("Number = 0");
+            //}
             #endregion
         }
     }
