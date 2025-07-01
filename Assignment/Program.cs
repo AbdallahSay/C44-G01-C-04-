1985b86465a72f37b5fe7c21910136c0e953c572
AbdallahSay <luka3122003@gmail.com>
2025-07-01 10:15:25 +0300

    Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-Write a program that takes a number from the user then print yes if  that number can be divided by 3 and 4 otherwise print no.
+
+            //Console.WriteLine("Please enter number");
+            //bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+            //if (isParsed &&( number % 3 == 0 || number % 4 ==0))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //    Console.WriteLine("No");
+            #endregion
+        }
+    }
+}
